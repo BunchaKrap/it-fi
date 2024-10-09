030e3d0894b891a55e23ef27f50e3e7394a4e6b2
Vukhile <145381967+Vukhile@users.noreply.github.com>
2024-10-09 15:46:31 +0300

    website v1.05

--- server.py
+++ server.py
@@ -243,7 +243,5 @@ def affiliates():
     )
 
 
-if __name__ == "__main__":
-    from waitress import serve
-
-    serve(app, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":  #for dev
+#     app.run()
