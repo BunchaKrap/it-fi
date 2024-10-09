91775a06cebdfa8b98971d67bbf37584e62cb434
Vukhile <145381967+Vukhile@users.noreply.github.com>
2024-10-09 15:43:32 +0300

    website v1.04

--- server.py
+++ server.py
@@ -244,4 +244,6 @@ def affiliates():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)  # remove debug for deployment
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=8080)
