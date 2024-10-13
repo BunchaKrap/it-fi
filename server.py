5744e3c8b9d217a81348bcb57805f74b6ab7d5f0
Vukhile <145381967+Vukhile@users.noreply.github.com>
2024-10-13 19:28:08 +0300

    website v1.07

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient  # noqa: F811
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
-import os
+# import os
 
 # import requests
 
